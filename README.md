03534694a4724866dece47d71b65f4f27aa8b44a
Lauri Paimen <lauri@paimen.info>
2012-09-02 19:54:39 +0300

    Improved documentation.

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # Browser globals
 
-Compares current properties of `window` global to contents of fresh `window` object, effectively listing introduced Javascript globals. This works only on a browser (no NodeJS etc).
+Compares current properties of `window` object to contents of fresh `window` object, effectively listing global Javascript variables from the page. This works only on a browser (no NodeJS etc).
+
+Tested on Firefox 15, Opera 12.02 and Chromium 18.
 
 ## Usage
 
-Include browserGlobals.js as the last file to the page and open inspector (Ctrl+Shift+I in most browsers).
+Include browserGlobals.js as the last file to the page and open inspector console (Ctrl+Shift+I in some browsers) to see the result.
 
-Alternately, copy-paste file contents to inspector console and run it (press Enter or Ctrl+Enter).
+Alternately, copy-paste contents of browserGlobals.js to inspector console and run it (press Enter or Ctrl+Enter).
 
 ## Example
 
-    // Surf to about:blank and open inspector
+    // Surf to about:blank and open inspector console
     > // Copy-paste and run the code
     New globals: 0 []
-    // Introduce a new global
+    // Introduce a new global:
     > newGlobal = 5;
     > // Copy-paste and run the code
     New globals: 1 ["newGlobal"]
--- browserGlobals.js
+++ browserGlobals.js
@@ -1,41 +1,49 @@
 (function(window, document) {
 
 // Document to push to iframe
+// Purpose is simply to listen for messages, evaluate the payload of it,
+// and return result to the caller.
 var iframeSource = 
     "<!DOCTYPE html>" +
     "<html><head><script>" +
     "window.addEventListener('message', function (e) {" +
     "   e.source.postMessage(eval(e.data), '*');" +
     "})</script></head></html>";
-var iframeDataUrl = "data:text/html;charset=utf-8," + escape(iframeSource);
 
-// Function to execute on iframe
+// Function to execute on iframe. Returns a list of properties of window
+// object, which is essentially a list of browser-defined global variables.
 var evalFunc = 
     "(function() {" +
     "   var i, n = [];" +
     "   for (i in window) {" +
     "       n.push(i);" +
     "   }" +
-    "   return n" +
+    "   return n;" +
     "})()";
 
-var iframe = document.createElement("iframe");
-iframe.onload = function () {
-    iframe.contentWindow.postMessage(evalFunc, '*');
-};
+// Setup listener for iframe reply message.
 function processReply(e) {
+
     window.removeEventListener("message", processReply);
+    document.body.removeChild(iframe);
+
+    // Compare globals of fresh window to globals of this window.
     var i, globals = [];
     for (i in window) {
         if (e.data.indexOf(i) == -1) {
             globals.push(i);
         }
     }
-    document.body.removeChild(iframe);
     window.console.log("New globals: " + globals.length, globals);
 }
 window.addEventListener("message", processReply, false);
+
+// Inject iframe
+var iframe = document.createElement("iframe");
+iframe.onload = function () {
+    iframe.contentWindow.postMessage(evalFunc, '*');
+};
 document.body.appendChild(iframe);
-iframe.src = iframeDataUrl;
+iframe.src = "data:text/html;charset=utf-8," + escape(iframeSource);;
 
 })(window, document);
